eb21d66cb49f12b65dadf964d8b0772ce81a78a1
Ali Yahya <amyahya@gmail.com>
2017-05-04 15:00:35 -0400

    Update metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@needle.com'
 license          'Apache 2.0'
 description      'Installs/Configures mongodb forked it so we can fix it and use it.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.6.0'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 
 depends 'runit'
 
